8e6fd1ad41af778577f053edeec83e75d8279241
Federico Di Marco <72440375+Allen13x@users.noreply.github.com>
2025-08-19 11:06:25 +0200

    Update PageRank.py

--- PageRank.py
+++ PageRank.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+### SMALL Graph Example ####
+
+
 # Define the adjacency matrix for the graph
 adj_matrix = np.array([
 	[0, 1, 0, 0],
@@ -35,3 +38,72 @@ print("PageRank values:")
 print(pagerank)
 # Output the number of iterations
 print(f"Number of iterations: {i}")
+
+#### Adding Links effect ####
+# Original Adjacency matrix
+adj_matrix = np.array([
+	[0, 1, 1, 0],
+	[0, 0, 1, 0],
+	[1, 0, 0, 0],
+	[0, 0, 1, 0]
+])
+
+# Number of nodes
+n = len(adj_matrix)
+
+# Damping factor
+damping_factor = 0.85
+
+# Initialize the PageRank values
+pagerank = np.ones(n) / n
+diff=1
+# Transition matrix
+transition_matrix= (1 - damping_factor) / n + damping_factor * (adj_matrix / adj_matrix.sum(axis=1, keepdims=True))
+# Iterative computation of PageRank
+i=0
+while diff > 1e-6: 
+	i+=1
+	pagerank1 = np.dot(transition_matrix.T, pagerank)
+	diff = np.linalg.norm(pagerank1 - pagerank)
+	pagerank = pagerank1
+
+# Output the PageRank values
+print("Original PageRank values:")
+print(pagerank)
+# Output the number of iterations
+print(f"Number of iterations: {i}")
+
+# Link added - Adjacency matrix
+adj_matrix = np.array([
+	[0, 1, 1, 1],
+	[0, 0, 1, 0],
+	[1, 0, 0, 0],
+	[0, 0, 1, 0]
+])
+
+# Number of nodes
+n = len(adj_matrix)
+
+# Damping factor
+damping_factor = 0.85
+
+# Initialize the PageRank values
+pagerank = np.ones(n) / n
+diff=1
+# Transition matrix
+transition_matrix= (1 - damping_factor) / n + damping_factor * (adj_matrix / adj_matrix.sum(axis=1, keepdims=True))
+# Iterative computation of PageRank
+i=0
+while diff > 1e-6: 
+	i+=1
+	pagerank1 = np.dot(transition_matrix.T, pagerank)
+	diff = np.linalg.norm(pagerank1 - pagerank)
+	pagerank = pagerank1
+
+# Output the PageRank values
+print("Link-added PageRank values:")
+print(pagerank)
+# Output the number of iterations
+print(f"Number of iterations: {i}")
+
+
